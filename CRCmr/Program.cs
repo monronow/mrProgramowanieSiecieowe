bb0cf46ac7ebf5958b44ba3899e4de07e049fec8
Lab1 <monronow@student.pg.edu.pl>
2017-11-13 19:22:15 +0100

    Lab1 PS

---
+++ CRCmr/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CRCmr
+{
+    class labjeden
+    {
+        static void Main()
+        {
+            
+            string slowo = "slowo";
+            int a, b, c = 0;
+            int suma;
+            suma=0;
+
+            for (int i = 0; i < slowo.Length; i++)
+            {
+                for (int j = 0; j < 8 ; j++)
+                {
+                    a = (1 << j);
+                    b = slowo[i];
+                    c = (c & a) >> j;
+
+                    suma ^= b;
+                }
+            }
+        }
+    }
+
+}
---
+++ CRCmr/przyklasd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRCm1r
+{
+    class Program
+    {
+        void Main(string[] args)
+        {
+            string imie;
+
+            Console.WriteLine("Podaj imię");
+            imie = Console.ReadLine();
+
+            Console.WriteLine("Masz na imię: {0}", imie);
+
+            Console.ReadLine();
+        }
+    }
+}
