a2ecb756e20d8749377ccd61688090e227a5ab50
monronow <32821499+monronow@users.noreply.github.com>
2018-01-08 13:08:18 +0100

    Add files via upload

---
+++ bit/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz 7-bitowa liczbe:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            string binValue = Convert.ToString(a, 2);
+
+            Console.WriteLine("\nbity: {0}", binValue);
+            Console.WriteLine("\nilosc bitow: {0}", binValue.Length);
+            int ilosc_bitow = binValue.Length;
+            Console.WriteLine("\nbit 0: {0}", binValue[0]);
+             
+            int akumulator = 0;
+            Console.WriteLine("\naku: {0}", akumulator);
+            for (int i = 0; i < ilosc_bitow; i++) {
+                akumulator ^=  binValue[i]-'0';
+               
+            }
+        Console.WriteLine("\nbit parzystosci: {0}", akumulator);
+              
+   
+ 
+           
+
+            Console.ReadKey();
+            }
+
+        }
+    }
